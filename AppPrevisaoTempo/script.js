f5faa9345808f1d5a50b6e32f636f5db05bc1bea
adsjinf <jinfrj@hotmail.com>
2024-10-26 08:54:00 -0300

    Implementado no script try carch para da um alerta informado que uma cidade não foi locazada quando receber uma resposta 404

--- AppPrevisaoTempo/script.js
+++ AppPrevisaoTempo/script.js
@@ -1,24 +1,37 @@
-import { config } from './config.js';
+import { config } from "./config.js";
 
 const key = config.API_KEY;
 
 function colocarDadosNaTela(dados) {
-  console.log(dados);
   document.querySelector(".tempo-cidade").innerHTML = "Tempo em " + dados.name;
   document.querySelector(".temperatura").innerHTML = Math.floor(dados.main.temp) + "ºC";
   document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description;
   document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%";
-  document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+  document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`;
 }
 
 async function buscarCidade(cidade) {
+  try {
+    const resposta = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&APPID=${key}&units=metric&lang=pt_br`);
+    
+    if (!resposta.ok) {
+        // Lança um erro se a cidade não for encontrada (exemplo: código 404)
+        throw new Error("Cidade não encontrada");
+    }
 
-  const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&APPID=${key}&units=metric&lang=pt_br`).then(resposta => resposta.json());
+    const dados = await resposta.json();
+    colocarDadosNaTela(dados);
 
-  colocarDadosNaTela(dados)
+} catch (error) {
+    // Exibe o alerta se houver erro (por exemplo, cidade não encontrada)
+    alert("Cidade informada não foi encontrada! Por favor, verifique o nome e tente novamente.");
+}
 }
 
 function cliqueiNoBotao() {
   const cidade = document.querySelector(".input-cidade").value;
-  buscarCidade(cidade)
+  buscarCidade(cidade);
 }
+
+// Adiciona o EventListener no botão
+document.querySelector(".botao-buscar").addEventListener("click", cliqueiNoBotao);
